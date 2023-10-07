5a1a022f530fb1c496c74d0897f03806bbee9495
lefkos <lefkos@medlo.se>
2023-10-07 17:33:29 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module'; // ? <-- Added
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule, // ? <-- Added
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
